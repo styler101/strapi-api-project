a246a9edd8161b53220acf3a7b4299974e895457
Lucas <lucas.santos@mundowap.com.br>
2023-03-30 07:06:32 -0300

    add strapi project

---
+++ config/env/production/plugins.js
@@ -0,0 +1,29 @@
+module.exports = ({ env }) => ({
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env("CLOUDINARY_NAME"),
+      api_key: env("CLOUDINARY_KEY"),
+      api_secret: env("CLOUDINARY_SECRET"),
+    },
+    actionOptions: {
+      upload: {},
+      delete: {},
+    },
+  },
+  email: {
+    provider: "nodemailer",
+    providerOptions: {
+      host: env("SMTP_HOST", "smtp-relay.sendinblue.com"),
+      port: env("SMTP_PORT", 587),
+      auth: {
+        user: env("SMTP_USERNAME"),
+        pass: env("SMTP_PASSWORD"),
+      },
+    },
+    settings: {
+      defaultFrom: "willianjustencursos@gmail.com",
+      defaultReplyTo: "willianjustencursos@gmail.com",
+    },
+  },
+});
